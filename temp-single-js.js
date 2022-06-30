ad6d28339c5533c3fb5362843684f60ee0bb27f4
vseveryday <105209021+vseveryday@users.noreply.github.com>
2022-06-30 15:21:59 +0700

    Add files via upload

--- temp-single-js.js
+++ temp-single-js.js
@@ -1,7 +1,15 @@
-var $page = $('html, body');
-$('a[href*="#"]').click(function() {
-    $page.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 400);
-    return false;
-});
+// var $page = $('html, body');
+// $('a[href*="#"]').click(function() {
+//     $page.animate({
+//         scrollTop: $($.attr(this, 'href')).offset().top
+//     }, 400);
+//     return false;
+// });
+$(function(){
+
+    $('.some-link').on('click', function(e){
+      $('html,body').stop().animate({ scrollTop: $('#top').offset().top }, 700);
+      e.preventDefault();
+    });
+    
+    });
